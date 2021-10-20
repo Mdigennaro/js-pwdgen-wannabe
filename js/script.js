59bb5006059a86e65fdde894807dc54f0175dd42
Mdigennaro <michele.digennaro719@gmail.com>
2021-10-20 15:54:25 +0200

    Fine esercizio

--- js/script.js
+++ js/script.js
@@ -8,3 +8,21 @@
 const firstname = prompt(`Qual è il tuo nome?`);
 const lastname = prompt(`Qual è il tuo cognome?`);
 const color = prompt(`Qual è il tuo colore preferito?`)
+
+console.log(firstname)
+console.log(lastname)
+console.log(color)
+
+document.getElementById(`stampa`).innerHTML =
+`
+Nome: ${firstname} <br>
+Cognome: ${lastname} <br>
+Colore preferito: ${color} <br>
+`
+const thisyear = 21;
+const password = firstname + lastname + color + thisyear;
+
+document.getElementById(`password`).innerHTML = 
+`
+Password: <strong>${password}</strong>
+`
