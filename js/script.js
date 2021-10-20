bb71b06cf90e186ab318354e1db960e4ad55e7d8
Mdigennaro <michele.digennaro719@gmail.com>
2021-10-20 17:30:37 +0200

    Aggiunta bonus completata

--- js/script.js
+++ js/script.js
@@ -2,27 +2,35 @@
 
 /*
 1. Inizializzo variabili con i dati
-2. Stampo dati
+2. Creazione automatica password
+3. Dichiarare n° caratteri password
+4. Stampo dati
 */
 
 const firstname = prompt(`Qual è il tuo nome?`);
 const lastname = prompt(`Qual è il tuo cognome?`);
-const color = prompt(`Qual è il tuo colore preferito?`)
+const color = prompt(`Qual è il tuo colore preferito?`);
 
-console.log(firstname)
-console.log(lastname)
-console.log(color)
+console.log(firstname);
+console.log(lastname);
+console.log(color);
 
 document.getElementById(`stampa`).innerHTML =
 `
 Nome: ${firstname} <br>
 Cognome: ${lastname} <br>
 Colore preferito: ${color} <br>
-`
+`;
+
 const thisyear = 21;
-const password = firstname + lastname + color + thisyear;
+let password = firstname + lastname + color + thisyear;
 
 document.getElementById(`password`).innerHTML = 
 `
 Password: <strong>${password}</strong>
+`;
+
+document.getElementById(`caratteri`).innerHTML = 
 `
+N° caratteri: ${password.length}
+`;
