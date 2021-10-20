5ceb36aa6c23a3d5d671a803ab5756e316e5050c
Mdigennaro <michele.digennaro719@gmail.com>
2021-10-20 15:25:26 +0200

    inserimento prompt

---
+++ js/script.js
@@ -0,0 +1,10 @@
+//inserisci nome, cognome e colore preferito//
+
+/*
+1. Inizializzo variabili con i dati
+2. Stampo dati
+*/
+
+const firstname = prompt(`Qual è il tuo nome?`);
+const lastname = prompt(`Qual è il tuo cognome?`);
+const color = prompt(`Qual è il tuo colore preferito?`)
